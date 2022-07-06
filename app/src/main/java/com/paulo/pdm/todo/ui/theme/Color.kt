580661ca70e3d5ff2c1bdaa56bf012f5b6d64057
opaulooo <o.paulooo.victor@gmail.com>
2022-07-05 23:36:37 -0300

    feat: ui theme, shape, color and type

---
+++ app/src/main/java/com/paulo/pdm/todo/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.paulo.pdm.todo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val AlmostWhite01 = Color(0xFFF5F5F5)
+val AlmostWhite02 = Color(0xFFE4E4E4)
+val AlmostWhite03 = Color(0xFFEFEFEF)
+val DarkGray = Color(0xFF585858)
+val AlmostBlack01 = Color(0xFF222222)
+val AlmostBlack02 = Color(0xFF121212)
+val AlmostBlack03 = Color(0xFF252525)
+val RedHeart = Color(0xFFF65151)
---
+++ app/src/main/java/com/paulo/pdm/todo/ui/theme/Theme.kt
@@ -0,0 +1,43 @@
+package com.paulo.pdm.todo.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = AlmostBlack01,
+    onPrimary = Color.White,
+    primaryVariant = AlmostBlack02,
+    secondary = AlmostBlack03,
+    onSecondary = Color.White
+)
+
+private val LightColorPalette = lightColors(
+    primary = AlmostWhite01,
+    onPrimary = Color.Black,
+    primaryVariant = AlmostWhite02,
+    secondary = AlmostWhite03,
+    onSecondary = Color.Black
+)
+
+@Composable
+fun ToDo(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
